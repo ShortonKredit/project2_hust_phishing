42c4c3f2a23570c5c49652f376ddb53f9983a394
Vinhhc <vinhconghoang5410@gmail.com>
2025-05-07 09:41:47 +0700

    add html file and clean csv 07/05

--- download_url_from_link.py
+++ download_url_from_link.py
@@ -8,5 +8,5 @@ urls = response.text.strip().split('\n')
 
 # Tạo DataFrame và lưu thành CSV
 df = pd.DataFrame(urls, columns=["phishing_url"])
-df.to_csv("openphish_feed_05_05.csv", index=False)
-print("Đã lưu file: openphish_feed_05_05.csv")
+df.to_csv("openphish_feed_07_05.csv", index=False)
+print("Đã lưu file: openphish_feed_07_05.csv")
--- filter_url.py
+++ filter_url.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
 # Đọc file CSV gốc
-df = pd.read_csv("html_mapping_2.csv")
+df = pd.read_csv("html_mapping_4_05_05.csv")
 
 # Lọc bỏ các dòng có 'ERROR' trong cột file_name
 df_filtered = df[~df['file_name'].str.contains("ERROR", na=False)]
 
 # Ghi đè lên file cũ
-df_filtered.to_csv("html_mapping_2.csv_filter", index=False)
+df_filtered.to_csv("html_mapping_4_05_05.csv", index=False)
 
-print("✅ Đã ghi đè file html_mapping_1.csv .")
+print("✅ Đã ghi đè file html_mapping.csv .")
